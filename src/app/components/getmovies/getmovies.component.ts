c5a5eca8e81ba61c3009cf60ed73bd208cae3d37
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-01-29 20:51:25 +0100

    nästan klar med logik

--- src/app/components/getmovies/getmovies.component.ts
+++ src/app/components/getmovies/getmovies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
+import { movieDetails } from 'src/app/models/movieDetails';
 
 @Component({
   selector: 'app-getmovies',
@@ -10,46 +11,53 @@ export class GetmoviesComponent implements OnInit {
 
   matchedMovies: Movie[] = []
 
-  filmcontainer:string = "filmcontainer"
+  detailedMovie: movieDetails = new movieDetails("", "", "", "", "", "", "", "", "")
+
+  filmcontainer: string = "filmcontainer"
+
+  detailsContainerStatus: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  movieSearch(titleToSearch:string){
+  movieSearch(titleToSearch: string) {
 
-    console.log(titleToSearch);
-    
-    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=5ed1c386&s=" + titleToSearch).then((response)=>response.json()
-    ).then((data)=>{
+    this.detailsContainerStatus = false
 
-      console.log(data.Search[0]);
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=5ed1c386&s=" + titleToSearch).then((response) => response.json()
+    ).then((data) => {
 
-      //this.matchedMovies = data.Search;
+      this.detailedMovie = new movieDetails("", "", "", "", "", "", "", "", "")
+
+      this.matchedMovies = []
 
       let searchResult = data.Search;
 
       for (let i = 0; i < searchResult.length; i++) {
         const thisFilm = searchResult[i];
 
-        this.matchedMovies.push(new Movie(thisFilm.Title,thisFilm.Poster,thisFilm.Year))
-        
+        this.matchedMovies.push(new Movie(thisFilm.Title, thisFilm.Poster, thisFilm.Year))
+
       }
 
     })
 
   }
 
-  renderMovieDetails(movieTitle:string){
+  renderMovieDetails(movieTitle: string) {
 
-    console.log(movieTitle);
+    this.detailsContainerStatus = true
 
-    fetch("http://www.omdbapi.com/?t="+movieTitle+"&apikey=5ed1c386&s").then((response)=>response.json()
-    ).then((data)=>{
+    fetch("http://www.omdbapi.com/?t=" + movieTitle + "&apikey=5ed1c386&s").then((response) => response.json()
+    ).then((data) => {
 
-      console.log(data);
-      
+      let selectedMovie = data;
+
+      this.matchedMovies = []
+
+      this.detailedMovie = new movieDetails(selectedMovie.Title, selectedMovie.Poster, selectedMovie.Genre, selectedMovie.Year, selectedMovie.Runtime, selectedMovie.Rated, selectedMovie.Plot, selectedMovie.Director, selectedMovie.Ratings[0].Source + ": " + selectedMovie.Ratings[0].Value)
 
     })
 
---
+++ src/app/models/movieDetails.ts
@@ -0,0 +1,27 @@
+export class movieDetails {
+
+    title:string
+    poster:string
+    genre:string
+    year:string
+    runtime:string
+    rated:string
+    plot:string
+    director:string
+    ratings:string
+    
+    constructor(title:string, poster:string, genre:string, year:string, runtime:string, rated:string, plot:string, director:string, ratings:string){
+
+        this.title = title
+        this.poster = poster
+        this.genre = genre
+        this.year = year
+        this.runtime = runtime
+        this.rated = rated
+        this.plot = plot
+        this.director = director
+        this.ratings = ratings
+
+    }
+
+}
